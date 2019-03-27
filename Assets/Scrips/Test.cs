23238788ac94467dafff1652c46ca2569989c0e0
Canghaizhulei <739044043@qq.com>
2019-03-27 23:47:44 +0800

    Update Test.cs

--- Assets/Scrips/Test.cs
+++ Assets/Scrips/Test.cs
@@ -10,7 +10,7 @@ public class Test : MonoBehaviour
 	void Start ()
 	{
 	    txt = transform.Find("Text").GetComponent<Text>();
-	    txt.text = "hello Mr.Jiang";
+	    txt.text = "hello Mr.Li";
 	}
 	
 	// Update is called once per frame
