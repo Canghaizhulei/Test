815904e424dc8d32a2538fc4a76f74d1b941d51a
logen <739044043@qq.com>
2019-03-27 23:30:40 +0800

    first commit

---
+++ Assets/Scrips/Test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+
+    private Text txt;
+	// Use this for initialization
+	void Start ()
+	{
+	    txt = transform.Find("Text").GetComponent<Text>();
+	    txt.text = "hello Mr.Jiang";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
